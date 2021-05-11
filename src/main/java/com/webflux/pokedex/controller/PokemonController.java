36807c5013007f21f86a56847c9247f90558d64d
cabralpriscila <priscilacabral.dev@gmail.com>
2021-05-11 17:26:18 -0300

    create endpoints for the app

---
+++ src/main/java/com/webflux/pokedex/controller/PokemonController.java
@@ -0,0 +1,78 @@
+package com.webflux.pokedex.controller;
+
+import com.webflux.pokedex.model.Pokemon;
+import com.webflux.pokedex.model.PokemonEvent;
+import com.webflux.pokedex.repository.PokemonRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+@RequestMapping("/pokemons")
+public class PokemonController {
+    private final PokemonRepository repository;
+
+    public PokemonController(PokemonRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Flux<Pokemon> getAllPokemons(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Pokemon>> getPokemonById(@PathVariable Long id){
+        return repository.findById(String.valueOf(id))
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Pokemon> savePokemon(@RequestBody Pokemon pokemon){
+        return repository.save(pokemon);
+    }
+
+    @PutMapping("{id}")
+    public Mono<ResponseEntity<Pokemon>> updatePokemon(@PathVariable(value = "id")Long id,@RequestBody Pokemon pokemon){
+        return repository.findById(String.valueOf(id))
+                .flatMap(existingPokemon -> {
+                    existingPokemon.setNome(pokemon.getNome());
+                    existingPokemon.setCategoria(pokemon.getCategoria());
+                    existingPokemon.setHabilidade(pokemon.getHabilidade());
+                    existingPokemon.setPeso(pokemon.getPeso());
+
+                    return repository.save(existingPokemon);
+                })
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<ResponseEntity<Object>> deletePokemon(@PathVariable(value = "id")Long id){
+        return repository.findById(String.valueOf(id))
+                .flatMap(existingPokemon ->
+                        repository.delete(existingPokemon)
+                            .then(Mono.just((ResponseEntity.ok().build())))
+                )
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping()
+    public Mono<Void> deleteAllPokemons(){
+        return repository.deleteAll();
+    }
+
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<PokemonEvent> getPokemonEvents(){
+        return Flux.interval(Duration.ofSeconds(5))
+                .map(val ->
+                        new PokemonEvent(val, "Pokemons"));
+    }
+}
